2ec56a5372985d134249a52f0d41bf0607e7efc0
Alex Platz <me@oddx.xyz>
2025-10-07 17:35:51 -0500

    functional api

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import express from 'express'
-// import bodyParser from 'body-parser'
 import connectDB from '../db/utils.js'
 import { getUrlById, postUrl } from './routes.js'
 
+const PORT = process.env.PORT || 3333
 const app = express()
 
 connectDB()
@@ -10,10 +10,9 @@ connectDB()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.get('/', (_, res) => res.status(200).json("I'm alive!"))
 app.get('/:urlId', getUrlById)
-app.post('/:origUrl', postUrl)
 
-const PORT = process.env.PORT || 3333
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
-})
+app.post('/', postUrl)
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
--- server/routes.js
+++ server/routes.js
@@ -6,18 +6,20 @@ import { validateUrl } from '../utils/utils.js'
 dotenv.config({ path: '../../.env' })
 
 export const getUrlById = async (req, res) => {
-  const urlId = req.params.id
+  const urlId = req.params.urlId
   const url = await Url.findOne({ urlId })
 
   if (url) {
-    updateClicks(urlId)
-    res.redirect(url.origUrl)
+    updateClicks(res, urlId)
+    // restore this with frontend
+    // res.redirect(url.origUrl)
+    res.status(200).json(url)
   } else {
     res.status(404).json('Not found')
   }
 }
 
-const updateClicks = async (urlId) => {
+const updateClicks = async (res, urlId) => {
   try {
     await Url.updateOne(
       { urlId },
@@ -30,32 +32,43 @@ const updateClicks = async (urlId) => {
 }
 
 export const postUrl = async (req, res) => {
-  const origUrl = req.params.origUrl
+  const { origUrl, urlId } = req.body
+
   if (validateUrl(origUrl)) {
     let url = await Url.findOne({ origUrl })
-    url ? res.json(url) : createShortUrl(origUrl)
+    url ?
+      res.json(url) :
+      createShortUrl(res, origUrl, urlId)
   } else {
     res.status(400).json('Invalid Original Url')
   }
 }
 
-const createShortUrl = async (origUrl) => {
-  const urlId = nanoid()
-  const base = process.env.BASE
-  const shortUrl = `${base}/urlId}`
-  const url = new Url({
-    origUrl,
-    shortUrl,
-    urlId,
-    date: new Date()
-  })
+const createShortUrl = async (res, origUrl, reqUrlId) => {
+  const doc = await Url.findOne({ urlId: reqUrlId })
 
-  try {
-    await url.save()
-    res.json(url)
-  } catch (err) {
-    console.log(err)
-    res.status(500).json('Server Error')
+  if (!doc) {
+    const urlId = reqUrlId ? reqUrlId : nanoid()
+    const base = process.env.BASE
+    const shortUrl = `${base}/${urlId}`
+    const url = new Url({
+      origUrl,
+      shortUrl,
+      urlId,
+      date: new Date()
+    })
+
+    try {
+      await url.save()
+      res.status(200).json(url)
+    } catch (err) {
+      console.log(err)
+      res.status(500).json('Server Error')
+    }
+  } else if (doc != origUrl) {
+    res.status(400).json('Provided shortUrl taken')
+  } else {
+    res.json(doc)
   }
 }
 
